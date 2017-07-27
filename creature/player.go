c4e8b8b7f83da1955008d074132a88ac99cbbf92
onorton <onorton125@gmail.com>
2017-07-27 23:46:08 +0000

    AC now printed in status and adjusts according to armour being worn.

--- creature/player.go
+++ creature/player.go
@@ -112,10 +112,11 @@ func (p *Player) TakeDamage(damage int) {
 	p.hp -= damage
 }
 func (p *Player) GetStats() []string {
-	stats := make([]string, 3)
+	stats := make([]string, 4)
 	stats[0] = fmt.Sprintf("HP:%d", p.hp)
 	stats[1] = fmt.Sprintf("STR:%d(%+d)", p.str, GetBonus(p.str))
 	stats[2] = fmt.Sprintf("DEX:%d(%+d)", p.dex, GetBonus(p.dex))
+	stats[3] = fmt.Sprintf("AC:%d", p.ac)
 	return stats
 }
 
