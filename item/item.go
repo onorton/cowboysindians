f4dcca9a07daa99c8737d8ac3100776abcf63813
onorton <onorton125@gmail.com>
2019-02-26 21:51:33 +0000

    Separate file created for "normal items" to separate interface logic from struct logic.

--- item/item.go
+++ item/item.go
@@ -1,14 +1,9 @@
 package item
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
-	"hash/fnv"
-	"io/ioutil"
 	"math/rand"
 
-	"github.com/onorton/cowboysindians/icon"
 	"github.com/onorton/cowboysindians/ui"
 )
 
@@ -18,47 +13,10 @@ func check(err error) {
 	}
 }
 
-type itemAttributes struct {
-	Icon        icon.Icon
-	Weight      float64
-	Cover       bool
-	Probability float64
-}
-
 type ItemDefinition struct {
 	Category string
 	Name     string
 	Amount   int
-}
-
-var normalItemData map[string]itemAttributes
-var normalItemProbabilities map[string]float64
-
-func fetchItemData() {
-	data, err := ioutil.ReadFile("data/item.json")
-	check(err)
-	var iD map[string]itemAttributes
-	err = json.Unmarshal(data, &iD)
-	check(err)
-	normalItemData = iD
-
-	normalItemProbabilities = make(map[string]float64)
-	for name, attributes := range normalItemData {
-		normalItemProbabilities[name] = attributes.Probability
-	}
-}
-
-type NormalItem struct {
-	name  string
-	ic    icon.Icon
-	w     float64
-	cover bool
-}
-
-func NewNormalItem(name string) Item {
-	item := normalItemData[name]
-	var itm Item = &NormalItem{name, item.Icon, item.Weight, item.Cover}
-	return itm
 }
 
 func SelectItem(probabilites map[string]float64) string {
@@ -80,67 +38,6 @@ func SelectItem(probabilites map[string]float64) string {
 	}
 	n := rand.Intn(len(items))
 	return items[n]
-}
-
-func GenerateNormalItem() Item {
-	return NewNormalItem(SelectItem(normalItemProbabilities))
-}
-
-func (item *NormalItem) MarshalJSON() ([]byte, error) {
-	buffer := bytes.NewBufferString("{")
-
-	nameValue, err := json.Marshal(item.name)
-	if err != nil {
-		return nil, err
-	}
-
-	buffer.WriteString(fmt.Sprintf("\"Name\":%s,", nameValue))
-
-	iconValue, err := json.Marshal(item.ic)
-	if err != nil {
-		return nil, err
-	}
-
-	buffer.WriteString(fmt.Sprintf("\"Icon\":%s,", iconValue))
-
-	weightValue, err := json.Marshal(item.w)
-	if err != nil {
-		return nil, err
-	}
-
-	buffer.WriteString(fmt.Sprintf("\"Weight\":%s,", weightValue))
-
-	coverValue, err := json.Marshal(item.cover)
-	if err != nil {
-		return nil, err
-	}
-
-	buffer.WriteString(fmt.Sprintf("\"Cover\":%s", coverValue))
-	buffer.WriteString("}")
-
-	return buffer.Bytes(), nil
-}
-
-func (item *NormalItem) UnmarshalJSON(data []byte) error {
-
-	type itemJson struct {
-		Name   string
-		Icon   icon.Icon
-		Weight float64
-		Cover  bool
-	}
-	var v itemJson
-
-	if err := json.Unmarshal(data, &v); err != nil {
-		return err
-	}
-
-	item.name = v.Name
-	item.ic = v.Icon
-	item.w = v.Weight
-	item.cover = v.Cover
-
-	return nil
 }
 
 type ItemList []Item
@@ -207,32 +104,6 @@ func (itemList *ItemList) UnmarshalJSON(data []byte) error {
 	*itemList = ItemList(items)
 
 	return nil
-}
-
-func (item *NormalItem) Render() ui.Element {
-	return item.ic.Render()
-}
-
-func (item *NormalItem) GetName() string {
-	return item.name
-}
-
-func (item *NormalItem) GetKey() rune {
-	h := fnv.New32()
-	h.Write([]byte(item.name))
-	key := rune(33 + h.Sum32()%93)
-	if key == '*' {
-		key++
-	}
-	return key
-}
-
-func (item *NormalItem) GetWeight() float64 {
-	return item.w
-}
-
-func (item *NormalItem) GivesCover() bool {
-	return item.cover
 }
 
 func GenerateItem() Item {
