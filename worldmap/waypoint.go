b06f6c8759078dd7111c862add142dd649c2e2de
onorton <onorton125@gmail.com>
2019-03-16 13:51:50 +0000

    Patrol waypoint system added where creatures will move between fixed waypoints in order.

--- worldmap/waypoint.go
+++ worldmap/waypoint.go
@@ -10,22 +10,39 @@ type RandomWaypoint struct {
 	currentWaypoint Coordinates
 }
 
-func NewRandomWaypoint(world *Map, location Coordinates) RandomWaypoint {
-	return RandomWaypoint{world, location}
+func NewRandomWaypoint(world *Map, location Coordinates) *RandomWaypoint {
+	return &RandomWaypoint{world, location}
 }
 
-func (w RandomWaypoint) NextWaypoint(location Coordinates) Coordinates {
+func (w *RandomWaypoint) NextWaypoint(location Coordinates) Coordinates {
 	if w.currentWaypoint == location {
 		for {
 			newX := location.X + rand.Intn(11) - 5
 			newY := location.Y + rand.Intn(11) - 5
-			if w.world.IsValid(newX, newY) && w.world.IsPassable(newX, newY) {
+			if w.world.IsValid(newX, newY) {
 				w.currentWaypoint = Coordinates{newX, newY}
 				break
 			}
 		}
 	}
 	return w.currentWaypoint
+}
+
+// Patrol means that creatures move between defined waypoints in order
+type Patrol struct {
+	waypoints []Coordinates
+	index     int
+}
+
+func NewPatrol(waypoints []Coordinates) *Patrol {
+	return &Patrol{waypoints, 0}
+}
+
+func (p *Patrol) NextWaypoint(location Coordinates) Coordinates {
+	if p.waypoints[p.index] == location {
+		p.index = (p.index + 1) % len(p.waypoints)
+	}
+	return p.waypoints[p.index]
 }
 
 type WaypointSystem interface {
